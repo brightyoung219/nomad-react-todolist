e71851c6f1999865e1778da2f1343fc6283b6906
brightyoung219 <brightyoung219@gmail.com>
2022-11-15 15:58:20 +0900

    form 다루기

---
+++ src/TodoList.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+function TodoList() {
+  const [toDo, setTodo] = useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget: {value} } = event;
+    setTodo(value);
+  }
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder='Write a to do'></input>
+        <button> Add </button>
+      </form>
+    </div>
+  )
+}
+
+export default TodoList;
